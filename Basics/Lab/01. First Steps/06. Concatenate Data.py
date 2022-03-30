a7f75662acc17c792fad4d61c25859087233b6b7
kotrabakoff <71779665+kotrabakoff@users.noreply.github.com>
2022-03-30 22:38:24 +0300

    Add files via upload

---
+++ Basics/Lab/01. First Steps/06. Concatenate Data.py
@@ -0,0 +1,6 @@
+name = input()
+family = input()
+age = int(input())
+town = input()
+
+print(f"You are {name} {family}, a {age}-years old person from {town}.")
---
+++ Basics/Lab/01. First Steps/08. Pet Shop.py
@@ -0,0 +1,9 @@
+dogs = int(input())
+pets = int(input())
+
+dogs_food = dogs * 2.5
+pets_food = pets * 4
+
+sum = (dogs_food + pets_food)
+
+print(f"{sum} lv.")
