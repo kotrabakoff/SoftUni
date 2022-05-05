492f04121dd2fe74dce2878a392e93fc58ff671f
kotrabakoff <71779665+kotrabakoff@users.noreply.github.com>
2022-05-05 21:05:26 +0300

    Delete Fundamentals/Lab/04. Functions directory

--- Fundamentals/Lab/04. Functions/05. Even Numbers.py
+++
@@ -1,9 +0,0 @@
-def Evens(a):
-    string = filter(lambda i: i % 2 == 0, a)
-    evens = list(string)
-    return evens
-
-
-numb_sequence = list(map(int, input().split(" ")))
-
-print(Evens(numb_sequence))
--- Fundamentals/Lab/04. Functions/07. Min Max and Sum.py
+++
@@ -1,8 +0,0 @@
-def Min_Max_Sum(a):
-    print(f"The minimum number is {min(a)}")
-    print(f"The maximum number is {max(a)}")
-    print(f"The sum number is: {sum(a)}")
-
-console = list(map(int, input().split()))
-
-Min_Max_Sum(console)
